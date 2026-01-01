29d140ed2e14c8e5702ff532bf9f3576a2e5f39a
u3190439002-tech <u3190439002@gmail.com>
2026-01-01 03:54:52 +0100

    Added folders and started working on stuff

---
+++ Forecasting/Needed_packages.R
@@ -0,0 +1,18 @@
+## Installing Packages ---------------------------------------------------
+install.packages("haven") #importing stata/SPSS data types
+install.packages("Hmisc") #you can take advantage of some labeling features
+install.packages("expss") #package integrates value labels support into base R
+install.packages("readr") # importing data csv types
+install.packages("dplyr") #data cleaning and manipulation
+install.packages("tidyverse") #data cleaning and manipulation#data cleaning and manipulation
+install.packages("forecast") #forecast related packages
+install.packages("fpp3") #forecast related packages
+install.packages("fpp2") #forecast related packages
+install.packages("lubridate") # works with dates
+install.packages("stargazer") #for nicer regression tables
+install.packages("modelsummary") #for model regression tables
+install.packages("estimatr") #for regressions with robust standard errors
+install.packages("ggplot2") # plots
+install.packages("zoo") # dates and other things
+install.packages("xts") # timeseries
+install.packages("data.table") # data manipulation
